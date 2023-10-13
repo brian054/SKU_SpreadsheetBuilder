a0f8632bbfe33a3c0eee3a2c6c38a5af52e42c1f
brian054 <brian.rowell54@gmail.com>
2023-10-13 12:36:59 -0500

    First commit

---
+++ JSON_to_CSV.py
@@ -0,0 +1,17 @@
+# 0. Slack onboarding to stop
+# 1. Download product and variant json's from Postman
+
+# 2. Download CSV from NetSuite (Could bookmark the custom page you need)
+
+# 3. Put csv's on google drive, then open them all
+
+# 4. Copy variant stuff under product stuff, then sort form A/Z
+
+# 5. Sort NS csv stuff
+
+# 6. Copy NS csv stuff to 3 cols right of product stuff
+
+# 7. Manually check the numbers.
+print("Hello World let's make our job a bit easier lol")
+
+# Let's build everything here unless it get's too crazy.
